9009f53c2955e23c454219e1b6f3d8bb179941da
jyli7 <jyl702@gmail.com>
2016-12-06 14:47:05 -0500

    register going up and down

--- elevators.py
+++ elevators.py
@@ -32,10 +32,25 @@ class Building(object):
 
 class Floor(object):
     def __init__(self):
-        self.num_passengers = 0
-        self.up_button_on = False
-        self.down_button_on = False
+        self._num_passengers_going_up = 0
+        self._num_passengers_going_down = 0
 
+    @property
+    def num_passengers_going_up(self):
+        return self._num_passengers_going_up
+
+    @property
+    def num_passengers_going_down(self):
+        return self._num_passengers_going_down
+
+    def add_passengers_up(self, num):
+        self._num_passengers_going_up += num
+
+    def add_passengers_down(self, num):
+        self._num_passengers_going_down += num
+
+    def total_passengers(self):
+        return self.num_passengers_going_up + self.num_passengers_going_down
 
 class Elevator(object):
     ELEVATOR_CAPACITY = 10
@@ -44,16 +59,19 @@ class Elevator(object):
         self.current_floor = 1
         self.capacity = Elevator.ELEVATOR_CAPACITY
         self.dest_queue = collections.deque
-        self.floor_buttons = []
+        self.num_passengers_to_floor = []
 
         for _ in range(num_floors):
             self.add_floor_button()
 
+    def get_num_passengers_total(self):
+        sum(self.num_passengers_to_floor)
+
     def add_floor_button(self):
-        self.floor_buttons.append(False)
+        self.num_passengers_to_floor.append(0)
 
     def remove_floor_button(self):
-        self.floor_buttons.pop()
+        self.num_passengers_to_floor.pop()
 
     def is_on_floor(self, floor_num):
         return self.current_floor == floor_num
@@ -65,12 +83,14 @@ class View(object):
         self.ELEVATOR_HEIGHT = 4
         self.ELEVATOR_WIDTH = 4
         self.WIDTH_BETWEEN_ELEVATORS = 2
+        self.UP_PASSENGER = u'\u2191'
+        self.DOWN_PASSENGER = u'\u2193'
 
     def get_floor_width(self):
         num_elevators = len(self.building.elevators)
         return (self.WIDTH_BETWEEN_ELEVATORS + self.ELEVATOR_WIDTH) * num_elevators + self.WIDTH_BETWEEN_ELEVATORS
 
-    def render_floor(self, elevator_indices, floor_num):
+    def render_floor(self, floor, elevator_indices, floor_num):
         num_elevators = len(self.building.elevators)
 
         # Print top line of floor
@@ -85,6 +105,7 @@ class View(object):
         print(" " * self.WIDTH_BETWEEN_ELEVATORS, end="")
         print("\n", end="")
 
+        # Print the sides of the elevator
         for _ in range(self.ELEVATOR_HEIGHT):
             for i in range(num_elevators):
                 if i in elevator_indices:
@@ -99,6 +120,11 @@ class View(object):
             print(" " * self.WIDTH_BETWEEN_ELEVATORS, end="")
             print("\n", end="")
 
+        # Print the passengers waiting on the floor
+        print(self.UP_PASSENGER * floor.num_passengers_going_up, end="")
+        print(self.DOWN_PASSENGER * floor.num_passengers_going_down, end="")
+        print("")
+
         print("_" * self.get_floor_width() + " Floor: {}".format(floor_num))
 
 
@@ -106,7 +132,29 @@ class View(object):
         for index, floor in enumerate(self.building.floors[::-1]):
             floor_num = self.building.get_num_floors() - index
             elevator_indices = [elevator_i for elevator_i, elevator in enumerate(self.building.elevators) if elevator.is_on_floor(floor_num)]
-            self.render_floor(elevator_indices, floor_num)
+            self.render_floor(floor, elevator_indices, floor_num)
+
+    def ask_for_input(self):
+        # Get num of passengers on each floor
+        for index, floor in enumerate(self.building.floors):
+            print("\n")
+            if index != len(self.building.floors) - 1:
+                new_num_going_up = int(input("Floor {}: There are {} passengers going UP here. How many new passengers going up? ".format(index + 1, floor.num_passengers_going_up)))
+                floor.add_passengers_up(new_num_going_up)
+                self.render()
+                print("Great, there are now {} passengers waiting to go up on floor {}".format(floor.num_passengers_going_up, index + 1))
+
+            if index > 0:
+                new_num_going_down = int(input("Floor {}: There are {} passengers going DOWN here. How many new passengers going down? ".format(index + 1, floor.num_passengers_going_down)))
+                floor.add_passengers_down(new_num_going_down)
+                self.render()
+                print("Great, there are now {} passengers waiting to go down on floor {}".format(floor.num_passengers_going_down, index + 1))
+
+        # occupied_elevators = [elevator for elevator in self.building.elevators if elevator.get_num_passengers_total > 0]
+        # # For any elevators that have passengers in them, have those passengers press buttons
+        # for index, elevator in enumerate(self.building.elevators):
+        #     if elevator.get_num_passengers_total() > 0:
+        #         int(input("Elevator {}: There are {} passengers here.".format(index + 1, elevator.get_num_passengers_total()))
 
 
 def main():
@@ -121,9 +169,9 @@ def main():
 
     view.render()
 
-    # while True:
-    #     ask_for_input()
-    #     render_building()
+    while True:
+        view.ask_for_input()
+        view.render()
 
 
 if __name__ == "__main__":
